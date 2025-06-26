ab57628670c834da70db3b6910c9e8289652ce86
stasbucik <53035783+stasbucik@users.noreply.github.com>
2025-06-26 11:55:42 +0200

    Fix image generation
    Signed-off-by: stasbucik <53035783+stasbucik@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -7,7 +7,10 @@ latest_file=$(ls -t project-spec/hw-description/*.bit 2>/dev/null | head -n 1)
 if [ -n "$latest_file" ]; then
     echo "Latest .bit file: $latest_file"
     petalinux-package --boot --fpga $latest_file --fsbl images/linux/zynq_fsbl.elf --u-boot --force
-    petalinux-package wic -w project-spec/meta-user/conf/rootfs.wks --wic-extra-args="-c xz" -o .
+    petalinux-package wic --wks project-spec/meta-user/conf/rootfs.wks \
+                          --rootfs-file images/linux/rootfs.tar.gz \
+                          --bootfiles "BOOT.BIN boot.scr image.ub" \
+                          --wic-extra-args="-c xz" -o .
 else
     echo "No .bit files found in ./project-spec/hw-description/"
 fi
